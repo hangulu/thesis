042becca41e79fcf539d3cb787893cebd06f4e09
Hakeem Angulu <hakeem.angulu@gmail.com>
2020-03-06 10:58:57 -0500

    refactor and improve prob_votes

--- code/prob_votes.py
+++ code/prob_votes.py
@@ -6,32 +6,30 @@ for ecological inference.
 import numpy as np
 import math
 import tensorflow as tf
-from tools import integer_partition, get_vote_pcts, permute_integer_partition
+import tools
 import scipy.special
 from tqdm import tqdm, trange
-from election import get_coefficients
+import election
 
-@tf.function
 def get_vote_probability(flat_index, grid, demo, coeff_dict):
     """
-    Find the probability of a grid's cell producing a
+    Find the probability of a PHC's cell producing a
     vote outcome of a given election for a candidate,
-    with a given probabilistic grid.
+    with a given PHC.
 
     flat_index (int): the flat index of the selected cell
-    grid (NumPy array): the probabilistic grid for the precinct
-    and candidate
+    grid (Tensor): the Tensor representation of a PHC
     demo (dict): the demographics of the district
     coeff_dict (dict): the binomial coefficients for each partition
 
-    return: the probability that a cell produced the observed outcome
+    return: the probability that a PHC's cell produced the observed outcome
     """
     # Find the corresponding index
     index = tf.unravel_index(flat_index, grid.shape)
     matrix_dim = grid.shape[0]
 
     # Find the vote percentages for each demographic group
-    vote_pcts = get_vote_pcts(index, matrix_dim, demo)
+    vote_pcts = election.get_vote_pcts(index, matrix_dim, demo)
 
     # Find the probability of the outcome
     total_prob = 0
@@ -70,19 +68,19 @@ def prob_votes(grid, demo, observed, coeff_dict):
     received in a given election, with a given
     probabilistic grid.
 
-    grid (NumPy array): the probabilistic grid for the precinct
-    and candidate
+    grid (Tensor): the Tensor representation of a PHC
     demo (dict): the demographics of the district
     observed (int): the observed number of votes the candidate received
     coeff_dict (dict): the binomial coefficients for each partition
 
-    return: the probability that a grid produced the observed outcomes
+    return: the probability that a PHC produced the observed outcomes
     """
-    flat_grid = tf.reshape(grid, [-1])
+    normalized_grid = tools.normalize(grid)
+    flat_grid = tf.reshape(normalized_grid, [-1])
     probs = enumerate(flat_grid)
     grid_prob = 1
     for flat_index, prob in probs:
-        vote_prob = get_vote_probability(tf.constant(flat_index), grid, demo, coeff_dict)
+        vote_prob = get_vote_probability(tf.constant(flat_index), normalized_grid, demo, coeff_dict)
         grid_prob *= (1 - (vote_prob * prob))
 
     return tf.math.log(1 - grid_prob)
