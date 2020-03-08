22af28ea5dac96d185df5421c98990b4bf8c338a
Hakeem Angulu <hakeem.angulu@gmail.com>
2020-03-07 22:16:11 -0500

    vectorize expec_votes and prepare it for mcmc

--- code/expec_votes.py
+++ code/expec_votes.py
@@ -5,63 +5,70 @@ for ecological inference.
 
 import numpy as np
 import tensorflow as tf
+import functools
+import tools
 
-def get_vote_outcome(flat_index, grid, demo, print_stats=False):
+def get_vote_outcome(flat_index, grid_shape, demo):
     """
     Find the vote outcome of a given election
     for a candidate, with a given probabilistic
     grid.
 
     flat_index (int): the flat index of the selected cell
-    grid (NumPy array): the probabilistic grid for the precinct
-    and candidate
+    grid_shape (Tensor): the shape of the PHC
     demo (dict): the demographics of the district
-    print_stats (boolean): whether to print the statistics
 
     return: the expectation of the vote outcome for that cell
     """
     # Find the corresponding index
-    index = np.unravel_index(flat_index, grid.shape)
-    matrix_dim = grid.shape[0]
+    index = tf.unravel_index(flat_index, grid_shape)
+    matrix_dim = grid_shape[0]
 
-    if print_stats:
-        print(f"The index is {index}.")
+    # Calculate the vote outcomes of the cell
+    demo_pop = np.fromiter(demo.values(), dtype=np.float32)
+    demo_vote_pcts = index / matrix_dim
 
-    # Calculate the vote outcomes given the cell selected
+    vote_outcome = tf.math.reduce_sum(demo_pop * tf.cast(demo_vote_pcts, tf.float32))
 
-    vote_outcome = np.zeros(len(demo))
+    return vote_outcome
 
-    for num, group in enumerate(demo):
-        # Find the probabilities the cell represents for each group
-        pct = index[num] / matrix_dim
-
-        if print_stats:
-            print(f"{int(pct * 100)}% of the {group} population voted for this candidate.")
-
-        vote_outcome[num] += demo[group] * pct
-
-    return np.sum(vote_outcome)
-
-
-def expec_votes(grid, demo, print_all_stats=False):
+@tf.function
+def expec_votes(grid, demo):
     """
     Find the expectation of the vote outcome
     for a candidate, with a given probabilistic
     grid.
 
-    grid (NumPy array): the probabilistic grid for the precinct
-    and candidate
+    grid (Tensor): the Tensor representation of a PHC
     demo (dict): the demographics of the district
 
     return: the expectation for the vote outcomes over the grid
     """
-    probs = tf.reshape(grid, [-1])
-    outcomes = []
-    for flat_index, prob in enumerate(probs):
-        # Set up printing
-        print_stats = False
-        if flat_index % 10 == 0 and print_all_stats:
-            print_stats = True
-        outcomes.append(get_vote_outcome(flat_index, grid, demo, print_stats))
+    normalized_grid = tools.normalize(grid)
+    flat_grid = tf.reshape(grid, [-1])
 
-    return np.average(outcomes, weights=probs)
+    get_vote_outcome_partial = functools.partial(
+        get_vote_outcome,
+        grid_shape=grid.shape,
+        demo=demo)
+
+    outcomes = tf.map_fn(get_vote_outcome_partial,
+        tf.range(tf.size(flat_grid)), dtype=tf.float32)
+
+    return tools.reduce_average(outcomes, flat_grid)
+
+@tf.function
+def prob_from_expec(grid, demo, observed):
+    """
+    Derive a probability of a PHC by comparing its expectation to
+    the observed number of votes.
+
+    grid (Tensor): the Tensor representation of a PHC
+    demo (dict): the demographics of the district
+    observed (int): the observed number of votes the candidate received
+
+    return: the complement of the sigmoid function applied to the
+    difference in expectation
+    """
+    return tf.math.log(1. - tf.math.sigmoid(
+        tf.math.abs(observed - expec_votes(grid, demo))))
